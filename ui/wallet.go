9b4f1e399909e433774dd2f49abddb07753d9e65
xenartist <146194585+xenartist@users.noreply.github.com>
2024-09-22 21:43:43 +1000

    create wallet form adjustment

--- ui/wallet.go
+++ ui/wallet.go
@@ -34,6 +34,9 @@ func CreateWalletUI(app *tview.Application) ModuleUI {
 
 			// After successful wallet creation, update the TextView
 			go func() {
+				// Hide createWalletForm
+				createWalletForm.Clear(true)
+
 				newPublicKey := utils.GLOBAL_PUBLIC_KEY
 				app.QueueUpdateDraw(func() {
 					publicKeyTextView := manageWalletForm.GetFormItemByLabel("Public Key").(*tview.TextView)
@@ -66,9 +69,17 @@ func CreateWalletUI(app *tview.Application) ModuleUI {
 	manageWalletForm.SetBorder(true).SetTitle("Manage Wallet")
 
 	// Create a flex layout for vertical arrangement
-	walletFlex := tview.NewFlex().SetDirection(tview.FlexRow).
-		AddItem(createWalletForm, 0, 1, true).
-		AddItem(manageWalletForm, 0, 1, false)
+	walletFlex := tview.NewFlex().SetDirection(tview.FlexRow)
+
+	if utils.GLOBAL_PUBLIC_KEY != "" {
+		// If GLOBAL_PUBLIC_KEY is not empty, only add manageWalletForm
+		walletFlex.AddItem(manageWalletForm, 0, 1, true)
+	} else {
+		// If GLOBAL_PUBLIC_KEY is empty, add both forms
+		walletFlex.
+			AddItem(createWalletForm, 0, 1, true).
+			AddItem(manageWalletForm, 0, 1, false)
+	}
 
 	// Add the flex layout to moduleUI
 	moduleUI.ConfigFlex.AddItem(walletFlex, 0, 1, true)
