183de445b358602c1b72d46a001540a72d725705
xenartist <146194585+xenartist@users.noreply.github.com>
2024-09-26 10:12:49 +1000

    fix execution path issue

--- utils/xoos.go
+++ utils/xoos.go
@@ -7,9 +7,17 @@ import (
 
 var GLOBAL_WORK_DIR string
 
+func getExecutablePath() string {
+	ex, err := os.Executable()
+	if err != nil {
+		panic(err)
+	}
+	return filepath.Dir(ex)
+}
+
 func XoosInit() {
 	var err error
-	GLOBAL_WORK_DIR, err = os.Getwd()
+	GLOBAL_WORK_DIR, err = os.Executable()
 	if err != nil {
 		// Handle error, can choose to panic or set a default value
 		panic("Unable to get current working directory: " + err.Error())
